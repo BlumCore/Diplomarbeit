6d7b94642d9914b2da761ab02564d272cb022215
Yannik Blum <yannikblum7@gmail.com>
2023-10-05 13:17:20 +0200

    added Heart icon and g-force grid

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ from backend.db import getItem, getLastItems
 app = Flask(__name__,  template_folder='templates')
 
 
-@app.route('/')
-def hello():
+@app.route('/', methods=["GET"])
+def index():
     item = getItem()
     lastimes = getLastItems()
     return render_template("index.html", item=item, old=lastimes)
+
+@app.route('/history')
+def history():
+    item = getItem()
+    lastimes = getLastItems()
+    return render_template("history.html", item=item, old=lastimes)
--- backend/db.py
+++ backend/db.py
@@ -12,13 +12,21 @@ dynamodb_client = boto3.client('dynamodb',
 dynamodb = boto3.resource('dynamodb', aws_access_key_id='AKIA36XSOWSAE6HGSR7A',
   aws_secret_access_key='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',
   region_name="eu-north-1")
+
 table = dynamodb.Table(TABLE_NAME)
 
 def getItem():
-  response = table.scan()
-  print(response["Items"][0])
+  response = table.query(
+    KeyConditionExpression=Key("device").eq("1"),
+    ScanIndexForward=False,
+    Limit=1
+  )
   return response["Items"][0]
 
 def getLastItems():
-    response = table.scan()
-    return response["Items"][1:11:1]
+  response = table.query(
+    KeyConditionExpression=Key("device").eq("1"),
+    ScanIndexForward=False,
+    Limit=10
+  )
+  return response["Items"]
