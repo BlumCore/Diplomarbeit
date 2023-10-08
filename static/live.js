b27e5f7c7801eed8f7ef47c451f3188413ee7fd7
Yannik Blum <yannikblum7@gmail.com>
2023-10-08 14:35:46 +0200

    added live data

--- static/live.js
+++ static/live.js
@@ -1,3 +1,5 @@
+const dot = document.getElementById("dot");
+const heart = document.getElementById("heart");
 
 const interval = setInterval(function() {
     const url = "http://127.0.0.1:5000/getItem"
@@ -5,5 +7,8 @@ const interval = setInterval(function() {
     .then(response => response.json())  
     .then(json => {
         console.log(json);
+        dot.style = "--x: " +  json.device_data.yacc + "; --y:" + json.device_data.yacc + ";";
+        heart.innerHTML = json.device_data.heartrate
+
     })
   }, 500);
