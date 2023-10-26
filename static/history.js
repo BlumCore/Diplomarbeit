175675c8585ffd351612d379715c1e5614ee58e2
Yannik Blum <yannikblum7@gmail.com>
2023-10-26 11:38:14 +0200

    fixed responsive design added Heartrate in History

--- static/history.js
+++ static/history.js
@@ -1,10 +1,12 @@
 const rows = document.querySelectorAll(".clickable-row")
 const dot = document.getElementById("dot")
+const heart = document.getElementById("heart_label")
 
 console.log(rows)
 rows.forEach((row) => {
     row.addEventListener("click", function(){
         console.log(row.children[0])
         dot.style = "--x: " +row.children[0].innerHTML + "; --y: " +row.children[1].innerHTML + ";"
+        heart.innerHTML = row.children[2].innerHTML;
     })
 });
